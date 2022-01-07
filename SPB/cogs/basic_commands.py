e7344e5d7fc19e1f746a5bca31c59a65afdbc49f
Fawreez <malfarizi42@gmail.com>
2022-01-07 10:32:49 +0700

    Add a banlist command The banlist command is used to display a list of banned members along with the reason into the chat

--- SPB/cogs/basic_commands.py
+++ SPB/cogs/basic_commands.py
@@ -34,6 +34,29 @@ class BasicCommands(commands.Cog):
 
             if (user.name, user.discriminator) == (member_name, member_discriminator):
                 await ctx.guild.unban(user)
+    
+    @commands.command()
+    async def banlist(self, ctx):
+        """Shows list of  banned members and the reason if available"""
+        banned_users = await ctx.guild.bans()
+        ban_list = []
+        count = 0
+
+        for ban_entry in banned_users:
+            count += 1
+
+            user = ban_entry.user
+            reason = ban_entry.reason
+
+            # Append string of banned member and their reason to a list
+            # ban_list is used to create tidy output
+            ban_list.append(f'{count}. Member: {user.name}#{user.discriminator}, Reason: {reason}')
+        
+        # Join each items in ban_list into a multiline string
+        output = '\n'.join(item for item in ban_list)
+        
+        await ctx.send(output)
+
 
 def setup(client):
     client.add_cog(BasicCommands(client))
